39a58c04dfd05d5be8354b904b3402977242c486
TudorOaneaTapp <tudor.oanea@tapptitude.com>
2024-10-18 12:47:59 +0300

    added history condensation in current query

--- chain/formatting.py
+++ chain/formatting.py
@@ -1,6 +1,15 @@
 import json
 
-def format_retrieved_documents(docs):
+def format_retrieved_documents(context):
+    query = context["query"]
+    docs = context["documents"]
+    print("---------------------------------------- FRD\n\n")
+
+    for doc in docs:
+        print(doc)
+        print()
+    print("\n---------------------------------------- FRD")
+
     formattedDocs = []
     strResult = ""
     for index, doc in enumerate(docs):
@@ -25,3 +34,18 @@ def format_reply(reply):
     print("---------- am ajuns in FR")
     
     return reply["reply"]
+
+def format_chat_history(chat_history):
+    # def estimate_tokens(text: str) -> int:
+    #     return (len(text) + 2) / 3
+    # maxTokens = 10000
+    
+    buffer = ""
+    for dialogue_turn in list(reversed(chat_history)):
+        human = "Human: " + dialogue_turn[0]
+        ai = "Assistant: " + dialogue_turn[1]#.split("\nRetrieved documents:\n[doc0]:")[0]
+        buffer = "\n" + "\n".join([human, ai]) + buffer
+
+        # if (estimate_tokens(buffer) > maxTokens):
+        #   break
+    return buffer
--- chain/rag_chain.py
+++ chain/rag_chain.py
@@ -4,20 +4,23 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_qdrant import QdrantVectorStore
 from langchain.retrievers import SelfQueryRetriever
 from langchain.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnableMap
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.chains.query_constructor.base import AttributeInfo
+from operator import itemgetter
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from chain.formatting import format_retrieved_documents, format_reply
+from chain.formatting import format_retrieved_documents, format_reply, format_chat_history
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-collection_name = os.environ["QDRANT_COLLECTION_NAME"]
+collection_name = "dedeman-collection2" # os.environ["QDRANT_COLLECTION_NAME"]
 answer_template_path = "resources/answer_prompt_template.txt"
+condensation_template_path = "resources/condensation_prompt_template.txt"
+top_k = 10
 
 qclient = qdrant_client.QdrantClient(
     url = os.environ["QDRANT_HOST"],
@@ -53,22 +56,52 @@ retriever = SelfQueryRetriever.from_llm(
     metadata_field_info=metadata_field_info,
     enable_limit=True,
     verbose=True,
-    search_kwargs={'k':5}
+    search_kwargs={'k':top_k}
 )
 
 with open(answer_template_path, "r") as f:
     _answer_template = f.read()
 ANSWER_PROMPT = ChatPromptTemplate.from_template(_answer_template)
 
-_answer_prompt_params = {
-    "context": retriever | format_retrieved_documents,
-    "question": RunnablePassthrough()
+with open(condensation_template_path, "r") as f:
+    _condansation_template = f.read()
+CONDENSE_QUESTION_PROMPT = ChatPromptTemplate.from_template(_condansation_template)
+
+# Definition of the RAG pipeline
+_condensation = RunnableMap(
+    initial_question = lambda x: x["question"],
+    standalone_question = { 
+        "question": lambda x: x["question"], 
+        "chat_history": lambda x: format_chat_history(x["chat_history"])
+    } | CONDENSE_QUESTION_PROMPT | llm | StrOutputParser()
+)
+
+_retrieval_context = {
+    "query": itemgetter("initial_question"),
+    "documents": itemgetter("standalone_question") | retriever
 }
 
-rag_chain = ( {
-    "query": RunnablePassthrough(), #lambda x: x["question"],
-    "reply": _answer_prompt_params | ANSWER_PROMPT | llm | StrOutputParser()
+_answer_prompt_params = {
+    "context": _retrieval_context | RunnableLambda(format_retrieved_documents),
+    "question": lambda x: x["standalone_question"],
+    # "retrieved_documents": ex_retrieved_documents,
+    # "retrieved_documents2": ex_retrieved_documents2,
+}
+
+history_chain = ({
+    "query": lambda x: x["question"], # RunnablePassthrough(),
+    "reply": _condensation | _answer_prompt_params | ANSWER_PROMPT | llm | StrOutputParser()
 } | RunnableLambda(format_reply))
+
+# _answer_prompt_params = {
+#     "context": retriever | format_retrieved_documents,
+#     "question": RunnablePassthrough()
+# }
+
+# rag_chain = ( {
+#     "query": RunnablePassthrough(), #lambda x: x["question"],
+#     "reply": _answer_prompt_params | ANSWER_PROMPT | llm | StrOutputParser()
+# } | RunnableLambda(format_reply))
 
 # testing
 # response = rag_chain.invoke("surub")
